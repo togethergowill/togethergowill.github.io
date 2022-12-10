95bf9a20b5e41681b5d908793acb8ddde407ab8a
togethergowill <99955796+togethergowill@users.noreply.github.com>
2022-12-10 18:23:03 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-	document.addEventListener('touchstart', function() {
-	    document.getElementById('audios').play()
-	})
-	  
-
-	 document.addEventListener('click', function() {
-	     document.getElementById('audios').play()
-	 })
