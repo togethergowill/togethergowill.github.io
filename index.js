8b605d79494778f5af1b16b431a40dface6b5d14
togethergowill <99955796+togethergowill@users.noreply.github.com>
2022-12-10 17:49:12 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+	document.addEventListener('touchstart', function() {
+	    document.getElementById('audios').play()
+	})
+	  
+
+	 document.addEventListener('click', function() {
+	     document.getElementById('audios').play()
+	 })
